6eaeb4070b62db6b25ad262127894a0c945479e3
Quentin <quentin.duchesn@live.fr>
2020-11-13 11:41:23 +0100

    test

--- src/components/SideMenu/SideMenu.js
+++ src/components/SideMenu/SideMenu.js
@@ -1,11 +1,11 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import {
-    PLAYER_BACKGROUND_COLOR,
-    PRIMARY_COLOR,
-    PROFILE_PIC_ADD_COLOR,
-    PROFILE_PIC_ADD_COLOR_HOVER,
-    USER_ROLE_COLOR
+  PLAYER_BACKGROUND_COLOR,
+  PRIMARY_COLOR,
+  PROFILE_PIC_ADD_COLOR,
+  PROFILE_PIC_ADD_COLOR_HOVER,
+  USER_ROLE_COLOR,
 } from "../../assets/theme/colors";
 import Divider from "@material-ui/core/Divider";
 import ListItemText from "@material-ui/core/ListItemText";
@@ -14,182 +14,185 @@ import ListItem from "@material-ui/core/ListItem";
 import List from "@material-ui/core/List";
 import Badge from "@material-ui/core/Badge";
 import Avatar from "@material-ui/core/Avatar";
-import AddIcon from '@material-ui/icons/Add';
+import AddIcon from "@material-ui/icons/Add";
 import IconButton from "@material-ui/core/IconButton";
-import PropTypes from 'prop-types';
-import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
-import PlayArrowIcon from '@material-ui/icons/PlayArrow';
-import PauseIcon from '@material-ui/icons/Pause';
-import SkipNextIcon from '@material-ui/icons/SkipNext';
-import CloseIcon from '@material-ui/icons/Close';
+import PropTypes from "prop-types";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import PlayArrowIcon from "@material-ui/icons/PlayArrow";
+import PauseIcon from "@material-ui/icons/Pause";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import CloseIcon from "@material-ui/icons/Close";
 
-const useStyles = makeStyles((theme) =>({
-    menuDiv: {
-        width: '250px',
-        height: '100vh',
-        backgroundColor: PRIMARY_COLOR,
-        borderRight: '1px solid rgba(0, 0, 0, 0.12)',
-        position: 'sticky',
-        top: 0,
+const useStyles = makeStyles((theme) => ({
+  menuDiv: {
+    width: "250px",
+    height: "100vh",
+    backgroundColor: PRIMARY_COLOR,
+    borderRight: "1px solid rgba(0, 0, 0, 0.12)",
+    position: "sticky",
+    top: 0,
+  },
+  profile: {
+    display: "flex",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    padding: "25px",
+  },
+  profileInfo: {
+    textAlign: "left",
+    marginLeft: "10px",
+    "&>h3": {
+      margin: 0,
     },
-    profile: {
-        display: 'flex',
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        padding: '25px',
+    "&>span": {
+      color: USER_ROLE_COLOR,
     },
-    profileInfo: {
-        textAlign: 'left',
-        marginLeft: '10px',
-        '&>h3': {
-            margin: 0,
-        },
-        '&>span': {
-            color: USER_ROLE_COLOR,
-        },
+  },
+  badge: {
+    "&>span>button": {
+      backgroundColor: PROFILE_PIC_ADD_COLOR,
+      "&:hover": {
+        backgroundColor: PROFILE_PIC_ADD_COLOR_HOVER,
+      },
     },
-    badge: {
-        '&>span>button': {
-            backgroundColor: PROFILE_PIC_ADD_COLOR,
-            '&:hover': {
-                backgroundColor: PROFILE_PIC_ADD_COLOR_HOVER,
-            },
-        },
+  },
+  badgeIcon: {
+    height: "10px",
+    width: "10px",
+  },
+  list: {
+    padding: "8px 0",
+    "&>a": {
+      paddingLeft: "25px",
+      paddingRight: "25px",
     },
-    badgeIcon: {
-        height: '10px',
-        width: '10px',
+  },
+  player: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    backgroundColor: PLAYER_BACKGROUND_COLOR,
+    maxHeight: "200px",
+    width: "100%",
+    textAlign: "center",
+    position: "relative",
+    "&>img": {
+      width: "80%",
     },
-    list: {
-        padding: '8px 0',
-        '&>a': {
-            paddingLeft: '25px',
-            paddingRight: '25px',
-        },
+  },
+  title: {
+    marginTop: "10px",
+    textAlign: "center",
+    "&>span": {
+      color: USER_ROLE_COLOR,
+      fontSize: "15px",
     },
-    player: {
-        position: 'absolute',
-        bottom: 0,
-        left: 0,
-        width: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    image: {
-        backgroundColor: PLAYER_BACKGROUND_COLOR,
-        maxHeight: '200px',
-        width: '100%',
-        textAlign: 'center',
-        position: 'relative',
-        '&>img': {
-            width: '80%',
-        },
-    },
-    title: {
-        marginTop: '10px',
-        textAlign: 'center',
-        '&>span': {
-            color: USER_ROLE_COLOR,
-            fontSize: '15px',
-        }
-    },
-    close: {
-        position: 'absolute',
-        top: 0,
-        right: 0,
-        color: PRIMARY_COLOR,
-        padding: 0,
-    },
-    displayNone: {
-        display: 'none',
-    }
+  },
+  close: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    color: PRIMARY_COLOR,
+    padding: 0,
+  },
+  displayNone: {
+    display: "none",
+  },
 }));
 
-const SideMenu = ({menu, selected}) => {
-    const classes = useStyles();
+const SideMenu = ({ menu, selected }) => {
+  const classes = useStyles();
 
-    const [playerRunning, setPlayerRunning] = useState(false);
-    const [player, setPlayer] = useState(false);
+  const [playerRunning, setPlayerRunning] = useState(false);
+  const [player, setPlayer] = useState(false);
 
-    const ListItemLink = (props) => {
-        return(
-            <ListItem button component={"a"} {...props}/>
-        )
-    };
+  const ListItemLink = (props) => {
+    return <ListItem button component={"a"} {...props} />;
+  };
 
-    const handlePlayerclick = (e) => {
-        e.preventDefault();
-        setPlayerRunning(!playerRunning);
-    };
+  const handlePlayerclick = (e) => {
+    e.preventDefault();
+    setPlayerRunning(!playerRunning);
+  };
 
-    return(
-        <>
-            <div className={classes.menuDiv}>
-                <div className={classes.profile}>
-                    <Badge
-                        className={classes.badge}
-                        overlap="circle"
-                        anchorOrigin={{
-                            vertical: 'bottom',
-                            horizontal: 'right',
-                        }}
-                        badgeContent={
-                            <IconButton aria-label="changer la photo" size={'small'}>
-                                <AddIcon className={classes.badgeIcon}/>
-                            </IconButton>
-                        }
-                    >
-                        <Avatar alt="Travis Howard" src={"https://i.pravatar.cc/200"} />
-                    </Badge>
-                    <div className={classes.profileInfo}>
-                        <h3>Guest Name</h3>
-                        <span>Utilisateur</span>
-                    </div>
-                </div>
-                <Divider />
-                <List className={classes.list}>
-                    {menu.map((item, index) => (
-                        <ListItemLink key={index} href={item.linkTo} selected={selected === item.label}>
-                            <ListItemIcon>{item.icon}</ListItemIcon>
-                            <ListItemText>{item.label}</ListItemText>
-                        </ListItemLink>
-                    ))}
-                </List>
-                <div className={player ? classes.player : classes.displayNone}>
-                    <div className={classes.image}>
-                        <img src="https://i.pravatar.cc/200" alt=""/>
-                        <IconButton aria-label="close" className={classes.close} >
-                            <CloseIcon/>
-                        </IconButton>
-                    </div>
-                    <div className={classes.title}>
-                        <h3>Cabeza</h3>
-                        <span>OBOY</span>
-                    </div>
-                    <div>
-                        <IconButton aria-label="previous">
-                            <SkipPreviousIcon />
-                        </IconButton>
-                        <IconButton aria-label="play/pause">
-                            {playerRunning
-                                ? <PauseIcon fontSize={"large"} onClick={handlePlayerclick}/>
-                                : <PlayArrowIcon fontSize={"large"} onClick={handlePlayerclick}/>
-                            }
-                        </IconButton>
-                        <IconButton aria-label="next">
-                            <SkipNextIcon />
-                        </IconButton>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className={classes.menuDiv}>
+        <div className={classes.profile}>
+          <Badge
+            className={classes.badge}
+            overlap="circle"
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "right",
+            }}
+            badgeContent={
+              <IconButton aria-label="changer la photo" size={"small"}>
+                <AddIcon className={classes.badgeIcon} />
+              </IconButton>
+            }
+          >
+            <Avatar alt="Travis Howard" src={"https://i.pravatar.cc/200"} />
+          </Badge>
+          <div className={classes.profileInfo}>
+            <h3>Guest Name</h3>
+            <span>Utilisateur</span>
+          </div>
+        </div>
+        <Divider />
+        <List className={classes.list}>
+          {menu.map((item, index) => (
+            <ListItemLink
+              key={index}
+              href={item.linkTo}
+              selected={selected === item.label}
+            >
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText>{item.label}</ListItemText>
+            </ListItemLink>
+          ))}
+        </List>
+        <div className={player ? classes.player : classes.displayNone}>
+          <div className={classes.image}>
+            <img src="https://i.pravatar.cc/200" alt="" />
+            <IconButton aria-label="close" className={classes.close}>
+              <CloseIcon />
+            </IconButton>
+          </div>
+          <div className={classes.title}>
+            <h3>Cabeza</h3>
+            <span>OBOY</span>
+          </div>
+          <div>
+            <IconButton aria-label="previous">
+              <SkipPreviousIcon />
+            </IconButton>
+            <IconButton aria-label="play/pause">
+              {playerRunning ? (
+                <PauseIcon fontSize={"large"} onClick={handlePlayerclick} />
+              ) : (
+                <PlayArrowIcon fontSize={"large"} onClick={handlePlayerclick} />
+              )}
+            </IconButton>
+            <IconButton aria-label="next">
+              <SkipNextIcon />
+            </IconButton>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 SideMenu.propTypes = {
-    menu: PropTypes.array.isRequired,
+  menu: PropTypes.array.isRequired,
 };
 
 export default SideMenu;
